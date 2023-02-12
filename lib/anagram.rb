06928a9fee00dae7843b525088e606346f851301
lizdmukami <lizdmukami@gmail.com>
2023-02-12 20:04:58 +0300

    commit tree 9a2110d6552c70cbb6df83f0cc7899b19f8503d2

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
-# Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        matches_in_the_array = []
+        array.each do |array_word|
+            if @word.length == array_word.length
+                if sort_letters(@word) == sort_letters(array_word)
+                    matches_in_the_array << array_word
+                end
+            end
+        end
+        matches_in_the_array
+    end
+
+    def sort_letters(word)
+        word.chars.sort.join
+    end
+end
+
+
